bca60f1cf185f081d1547dc47c4a2573f13c77c3
sdshuvo98 <hellosdshuvo@gmail.com>
2022-08-11 17:38:46 +0600

    login page from team-srv

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's build it together!</h1>
+      <Login />
     </div>
   );
 }
