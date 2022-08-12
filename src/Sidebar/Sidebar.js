da7f69fa46fcd7fe9904db0e95946006b0216cd0
ekta gupta <ektagupta2211@gmail.com>
2022-08-12 19:04:09 +0530

    done with the sidebar

---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOptions from "./SidebarOptions";
+
+import TwitterIcon from "@mui/icons-material/Twitter";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <TwitterIcon className="sidebar__twitterIcon" />
+
+      <SidebarOptions active Icon={HomeIcon} text="Home" />
+      <SidebarOptions Icon={SearchIcon} text="Explore" />
+      <SidebarOptions Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOptions Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOptions Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOptions Icon={ListAltIcon} text="Lists" />
+      <SidebarOptions Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOptions Icon={MoreHorizIcon} text="More" />
+
+      
+      
+    </div>
+  );
+}
+
+export default Sidebar;
