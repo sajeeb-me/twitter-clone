6aa8fb3dd34296f5a8273a47369d0ae058a174bf
sdshuvo98 <hellosdshuvo@gamil.com>
2022-08-24 13:09:00 +0600

    assigend task for building profile pages

---
+++ src/pages/Profile/MainProfile/MainProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainProfile = () => {
+    return (
+        <div>
+            <h1>Sravani will work at Main profile</h1>
+        </div>
+    );
+};
+
+export default MainProfile;
